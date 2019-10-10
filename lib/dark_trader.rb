f791dc7ae9e17eb98e1eb13cefcdb8b8f039c288
Pak <andrei-pak@outlook.fr>
2019-10-11 00:17:01 +0200

    edit dark_trader and mairie_christmas

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -3,10 +3,10 @@ require 'nokogiri'
 require 'open-uri'
    
 def crypto_price
-tab_price = []
-page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-page.xpath('//td/a[@class ="price"]').each do |price|    
-tab_price << price.text
+ tab_price = []
+ page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+ page.xpath('//td/a[@class ="price"]').each do |price| #Xpath qui permet d'aller chercher tous les liens répértoriés sous la class "price"
+ tab_price << price.text #On garde uniquement le texte des liens récupérés plus haut et on les push dans l'array tab_price
  end
  return tab_price
 end
@@ -14,8 +14,8 @@ end
 def crypto_name
   tab_name = []
   page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-  page.xpath('//td//span/a[@class ="link-secondary"]').each do |name| 
-  tab_name << name.text
+  page.xpath('//td//span/a[@class ="link-secondary"]').each do |name| #Xpath qui permet d'aller chercher tous les liens répértoriés sous la class "link-secondary"
+  tab_name << name.text #On garde uniquement le texte des liens récupérés plus haut et on les push dans l'array tab_name
    end
    return tab_name
 end
@@ -24,11 +24,11 @@ def perform
   tab_end = []
   tab_price = crypto_price
   tab_name = crypto_name
-  hash = Hash[tab_name.zip tab_price]
-  hash.each do |a, b|
+  hash = Hash[tab_name.zip tab_price] #Cette ligne nous permet de "zip" 2 arrays dans un seul hash
+  hash.each do |nom, valeur| #Dans le hash créé plus haut pour chaque "key,value" nom = nom de la crypto et b = sa valeur 
     myhash = {}
-    myhash[a] = b
-    tab_end << myhash
+    myhash[nom] = valeur
+    tab_end << myhash #On push myhash dans le array tab_end
   end
   puts "#{tab_end}"
 end
--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -2,21 +2,21 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
    
-def get_townhall_email(url)
-  list = Hash.new
+def get_townhall_email(url) #Récupérer l'email de la marie depuis l'URL http://annuaire-des-mairies.com/NOM_DE_LA_VILLE
+  list = {}
   url = url[1..-1]
-  page = Nokogiri::HTML(open('http://annuaire-des-mairies.com' + url)) # On prend l'URL de base et on ajoute la partie manquante de l'URL afin de récupérer l'email de chaque mairie
-  email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+  page = Nokogiri::HTML(open('http://annuaire-des-mairies.com' + url)) #On prend l'URL de base et on ajoute la partie manquante de l'URL afin de récupérer l'email de chaque mairie
+  email = page.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]").text #Xpath qui permet d'aller chercher l'élèment "email" sur la page
 end
 
 def get_townhall_urls
-  urls = Array.new
+  urls = []
   page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
-  arr = page.xpath("//a[@class='lientxt']")
+  arr = page.xpath("//a[@class='lientxt']") #Xpath qui permet d'aller chercher le NOM_DE_LA_VILLE pour chaque ville
   arr.each do |link_name|
-    list = Hash.new
+    list = {}
     txt = link_name.text
-  email = get_townhall_email(link_name['href'])
+  email = get_townhall_email(link_name['href']) #NOM_DE_LA_VILLE est ajouté à l'URL http://annuaire-des-mairies.com/ afin de récupérer l'email de chaque ville
   list["nom"] = txt
   list["email"] = email
   urls << list
